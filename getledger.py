da7296419a3eac29c4ca28ea866364d8a67911e6
2w00fs <63689305+2w00fs@users.noreply.github.com>
2022-09-25 01:35:06 +0000

    simplified getledger process + styling

--- getledger.py
+++ getledger.py
@@ -1,47 +1,65 @@
-from kucoin.client import Market
 import time
 import credentials
-from orderclass import Order
-
+import json
 from kucoin.client import User # Ledger data stored accessed through User endpoint in API and its SDK
+
 client = User(credentials.api_key, credentials.api_secret, credentials.api_passphrase) # Create User class called client
+name = input("Ledger filename pls: ")
+data = []
 
-def getledger(start,end,page):
-    try:
-        ledger = client.get_account_ledger(startAt=start,endAt=end,pageSize=500,currentPage=page)
-    except:
-        return -1
-    else:
-        #print(ledger)
-        return ledger['totalPage']
+with open("log2.txt", 'a') as error_log:
+    pass
 
+def get_ledger(start,end,page):
+    while True:
+        try:
+            ledger = client.get_account_ledger(startAt=start,endAt=end,pageSize=500,currentPage=page)
+        except Exception as e:
+            if "Too Many Requests" in str(e.args):
+                print(f"Too many requests, paused at {utc(start)} will retry again in 10 seconds")
+                for i in range(1,11,1):
+                    print(f"Retrying in {i}", end='\r')
+                    time.sleep(1)
+                print("...RETRYING...")
+            else:
+                print()
+                print(f"FAILED TO FETCH PRICE AT {utc(start)}")
+                print(e)
+                error_log.write(str(start))
+                error_log.wrtie(e)
+                return 20000
+        else:
+            data.append(ledger)
+            return ledger['totalPage']
 
+def utc(ca):
+    createdat = int(ca / 1000)
+    local_time = time.strftime('%d/%m/%Y', time.localtime(createdat))
+    return local_time
 
-startat = 1603745599999
-dynamicend = startat + 86399999
-endat = 1610075199810
+# 1583020800000 1659139200000
+# FY 21 22 1625097600000 1656633600000
+def main(startat,endat):
+    for day_start_time in range(startat, endat + 86399999, 86399999):
+        day_end = day_start_time + 86399998
+        print(utc(day_start_time), '\r') # prints date in UTC instead of UNIX
+        page = 1
+        total_pages = 2
+        while page <= total_pages:
+            total_pages = get_ledger(day_start_time,day_end,page)
+            print(f"Page {page} of {total_pages}", end='\r')
+            page += 1
+            time.sleep(0.2) # wait for 0.2 seconds
 
-
-while dynamicend <= endat: # doesnt use a for loop because i want it to be able to repeat in case of error
-    pagenumber = 1
-
-    ledger = getledger(startat,dynamicend,pagenumber)
-    print(ledger)
+    with open(f"{name}.json", "w") as outfile:
+        json.dump(data, outfile)
     
-    if ledger >= 0:
-        if ledger > 1:
-            while pagenumber <= ledger:
-                ledger = getledger(startat,dynamicend,pagenumber)
-                if ledger >= 0:
-                    pagenumber += 1
-                    time.sleep(0.2) # wait for 0.2 seconds
-                else:
-                    time.sleep(1) # wait for 1 second
-        
-        startat = dynamicend
-        dynamicend = startat + 86399999
-        time.sleep(0.2) # wait for 0.2 seconds
-    else:
-        time.sleep(1) # wait for 1 second
-    
+if __name__ == "__main__":
+    main(1622910800000,1625029200000)
 
+"""
+    startat = 1583020800000
+    dynamicend = startat + 86399999
+    endat = 1659139200000
+    main(1583020800000,1659139200000)
+"""
