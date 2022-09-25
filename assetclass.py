cdff70b913b47f3ecf8669c9d52a60d192b84950
2w00fs <63689305+2w00fs@users.noreply.github.com>
2022-09-25 05:53:20 +0000

    rounded values

--- assetclass.py
+++ assetclass.py
@@ -1,16 +1,22 @@
 class Asset:
-    def __init__(self, asset, size):
-        self.value = (asset,size)
+    def __init__(self, asset, size, usdvalue):
+        self.value = (asset,size, usdvalue)
     
     @property
     def value(self):
-        return {"asset": self.__asset, "size": self.__size}
+        return {
+        "asset": self.__asset,
+        "size": self.__size,
+        "usdvalue": self.__usdvalue
+        }
     
     @value.setter
     def value(self, value_dict):
-        asset, size = value_dict
+        asset, size, usdvalue = value_dict
         self.__asset = asset
-        self.__size = size
+        self.__size = round(size,5)
+        self.__usdvalue = round(usdvalue,2)
 
-    def add(self, size):
-        self.__size += size
+    def add(self, size, usdvalue):
+        self.__size += round(size,5)
+        self.__usdvalue += round(usdvalue,2)
